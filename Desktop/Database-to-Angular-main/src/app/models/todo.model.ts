acc603cb3d78c00c425556eaf9c4e5a1d6a918c3
alchris15 <134898906+alchris15@users.noreply.github.com>
2023-08-18 13:37:54 +0800

    Delete Desktop directory

--- Desktop/Database-to-Angular-main/src/app/models/todo.model.ts
+++
@@ -1,9 +0,0 @@
-export class TodoModel {
-    public id!:number;
-    public todolist!:string;
-    public isCompleted!:number;
-    public isDeleted!:number;
-    public completed!:boolean;
-    
-    }
-    
--- Desktop/New folder/Database-to-Angular-main/Desktop/Database-to-Angular-main/src/app/models/Todo.ts
+++
@@ -1,5 +0,0 @@
-export class Todo {
-    content!: string;
-    completed!:boolean;
-    
-}
